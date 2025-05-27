62fcd37cab6b57e8916baea5dc7f2f1ff7ebb069
Lopesmatheus1 <lopesmatheus1@hotmail.com>
2025-05-27 19:28:07 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello wordl!</h1>
+      <h1 className="text-3xl font-bold text-red-500 underline">
+        Hello wordl!
+      </h1>
+      <Button>Click me</Button>
     </div>
   );
 }
